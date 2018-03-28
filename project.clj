d81e8767b9566a1e6f1071a2397fad85f5a55198
koran023 <35508097+koran023@users.noreply.github.com>
2018-03-28 15:02:31 -0500

    Added dependency for clj-uuid

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [iota "1.1.3"]
-                 [me.raynes/fs "1.4.6"]]
+                 [me.raynes/fs "1.4.6"]
+                 [danlentz/clj-uuid "0.1.7"]]
   :main ^:skip-aot edn2csv.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
